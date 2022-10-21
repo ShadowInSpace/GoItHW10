273fa4a00001696140d496b491b99056a13ea5bb
ShadowInSpace <ivan.yackovec@gmail.com>
2022-10-21 16:18:57 +0300

    First comlite

--- src/main/java/PhoneReader.java
+++ src/main/java/PhoneReader.java
@@ -6,19 +6,19 @@ import java.util.regex.Pattern;
 
 public class PhoneReader {
     public void read(File file) throws IOException {
-        InputStream fis = new FileInputStream(file);
-        Scanner scanner = new Scanner(fis);
-        Pattern pattern = Pattern.compile("[\\(]{0,1}[0-9]{3}[\\)]{0,1}[-\\s\\.]?[0-9]{3}[-]?[0-9]{4}$");
+        try (InputStream fis = new FileInputStream(file);
+             Scanner scanner = new Scanner(fis)) {
+            Pattern pattern = Pattern.compile("[\\(]{0,1}[0-9]{3}[\\)]{0,1}[-\\s\\.]?[0-9]{3}[-]?[0-9]{4}$");
 
-        while (scanner.hasNext()) {
-            String str = scanner.nextLine();
-            Matcher matcher = pattern.matcher(str);
-            if(matcher.find()){
-                System.out.println(str);
+            while (scanner.hasNext()) {
+                String str = scanner.nextLine();
+                Matcher matcher = pattern.matcher(str);
+                if (matcher.find()) {
+                    System.out.println(str);
+                }
+
             }
-
+        } catch (IOException e) {
         }
-        String line = scanner.nextLine();
-        fis.close();
     }
 }

0826c0e95b2a4193bd40505c6a89bbf6a868dd72
ShadowInSpace <ivan.yackovec@gmail.com>
2022-10-25 00:52:22 +0300

    All done, commented.

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -3,12 +3,15 @@ import java.io.IOException;
 
 public class MainTest {
     public static void main(String[] args) {
+        //Test for first task
         File file = new File("test.txt");
         PhoneReader01 phoneReader01 = new PhoneReader01();
         phoneReader01.read(file);
 
+        //Test for second task
         Rewriter.rewrite();
 
+        //Test for third task
         Calculator calculator = new Calculator();
         calculator.calculate();
     }
